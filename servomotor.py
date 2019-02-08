804814d3acc7193b3e4cf2529e997dc8f84d39d5
lvndry <monga.4landry@gmail.com>
2019-02-08 17:00:23 +0100

    single rotation

--- servomotor.py
+++ servomotor.py
@@ -1,40 +1,24 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.OUT)
-GPIO.setwarnings(False)
-
+FREQUENCE = 100
+SERVO_PIN = 17
 ajoutAngle = 5
 
-print("\n+----------/ ServoMoteur  Controlleur /----------+")
-print("|                                                |")
-print("| Le Servo doit etre branche au pin 11 / GPIO 17 |")
-print("|                                                |")
-print("+------------------------------------------------+\n")
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SERVO_PIN, GPIO.OUT)
+GPIO.setwarnings(False)
 
+print("\n+----------/ ServoMotor controllor /----------+")
+print("|                                               |")
+print("|   Servo must be plugged on pin 11 / GPIO 17   |")
+print("|                                               |")
+print("+-----------------------------------------------+\n")
 
-pwm=GPIO.PWM(17,100)
+pwm = GPIO.PWM(SERVO_PIN, FREQUENCE)
 pwm.start(5)
-
-angle1 = 0
-duty1 = float(angle1)/10 + ajoutAngle
-
-angle2=360
-duty2= float(angle2)/10 + ajoutAngle
-
-pwm.ChangeDutyCycle(duty1)
+duty = float(angle2)/10 + ajoutAngle
+pwm.ChangeDutyCycle(duty)
 time.sleep(0.8)
-pwm.ChangeDutyCycle(duty2)
-time.sleep(0.8)
-GPIO.cleanup()
 
-angle = 60
-
-pwm=GPIO.PWM(17,100)
-pwm.start(5)
-
-angleChoisi = angle/10 + ajoutAngle
-pwm.ChangeDutyCycle(angleChoisi)
-time.sleep(1.5)
 GPIO.cleanup()
