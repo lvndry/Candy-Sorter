bac3b604d5cf0d5c55f785fdd69f3673ee1918f1
lvndry <monga.4landry@gmail.com>
2019-02-13 09:57:23 +0100

    +60 angle rotation

--- servomotor.py
+++ servomotor.py
@@ -1,8 +1,9 @@
 import RPi.GPIO as GPIO
 import time
 
-FREQUENCE = 100
 SERVO_PIN = 17
+FREQUENCE = 100
+DUTY_CYCLE = 7.5
 ajoutAngle = 5
 
 GPIO.setmode(GPIO.BCM)
@@ -10,15 +11,18 @@ GPIO.setup(SERVO_PIN, GPIO.OUT)
 GPIO.setwarnings(False)
 
 print("\n+----------/ ServoMotor controllor /----------+")
-print("|                                               |")
+print("|                Candy - Sorter                 |")
 print("|   Servo must be plugged on pin 11 / GPIO 17   |")
 print("|                                               |")
 print("+-----------------------------------------------+\n")
 
 pwm = GPIO.PWM(SERVO_PIN, FREQUENCE)
-pwm.start(5)
+pwm.start(DUTY_CYCLE)
 duty = float(angle2)/10 + ajoutAngle
-pwm.ChangeDutyCycle(duty)
-time.sleep(0.8)
+GPIO.output(17, True)
+pwm.ChangeDutyCycle(DUTY_CYCLE)
+time.sleep(1) # 60 degree
+GPIO.output(17, False)
+pwm.ChangeDutyCycle(0)
 
 GPIO.cleanup()
