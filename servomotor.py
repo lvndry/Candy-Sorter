7fff01f7c649d92084d4fa7d5d7a56f8528ed80d
lvndry <monga.4landry@gmail.com>
2019-03-26 11:47:11 +0100

    return to origin

--- servomotor.py
+++ servomotor.py
@@ -1,28 +1,38 @@
 import RPi.GPIO as GPIO
 import time
 
-SERVO_PIN = 17
-FREQUENCE = 100
-DUTY_CYCLE = 7.5
-ajoutAngle = 5
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(SERVO_PIN, GPIO.OUT)
-GPIO.setwarnings(False)
-
-print("\n+----------/ ServoMotor controllor /----------+")
+print("\n+----------/ Servomotor controller /----------+")
 print("|                Candy - Sorter                 |")
 print("|   Servo must be plugged on pin 11 / GPIO 17   |")
 print("|                                               |")
 print("+-----------------------------------------------+\n")
 
-pwm = GPIO.PWM(SERVO_PIN, FREQUENCE)
-pwm.start(DUTY_CYCLE)
-duty = float(angle2)/10 + ajoutAngle
-GPIO.output(17, True)
-pwm.ChangeDutyCycle(DUTY_CYCLE)
-time.sleep(1) # 60 degree
-GPIO.output(17, False)
-pwm.ChangeDutyCycle(0)
+SERVO_PIN = 17
+GOBELET = 1
+ANGLE = 60 * GOBELET
+FREQUENCY = 50
+START_FC = 50
+DUTY_CYCLE = 5
+ROTATION_TIME = 1.5
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SERVO_PIN, GPIO.OUT)
+GPIO.setwarnings(False)
+GPIO.output(17, True)
+
+pwm = GPIO.PWM(SERVO_PIN, FREQUENCY)
+pwm.start(START_FC)
+angle = (float(ANGLE)/180 + 1) * 5
+pwm.ChangeDutyCycle(angle)
+time.sleep(ROTATION_TIME) # 60 degree
+
+pwm.ChangeDutyCycle(0)
+time.sleep(2)
+
+origin = (((360 - ANGLE) / 180) + 1) * 5
+pwm.ChangeDutyCycle(origin)
+time.sleep(ROTATION_TIME)
+
+pwm.stop()
+GPIO.output(17, False)
 GPIO.cleanup()
